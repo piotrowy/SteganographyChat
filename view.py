b37aa5b17319391ea41d425e0166de2f9a002991
Piotr Ceranek <petr.ceranek@gmail.com>
2016-01-23 00:30:42 +0100

    GUI development

--- view.py
+++ view.py
@@ -1,32 +1,44 @@
 import tkinter as tk
+from tkinter import *
 __author__ = 'piotrowy'
 
 
 class Chat(tk.Frame):
-    button_send = None,
-    button_server = None,
-    button_quit = None
 
-    def __init__(self, master=None):
+    def __init__(self, master):
         tk.Frame.__init__(self, master)
         self.pack()
         self.set_meta_view(master)
-        self.create_widgets(master)
+        self.create_widgets()
 
     @staticmethod
     def set_meta_view(master) -> object:
         master.title("Client")
-        master.minsize(400, 300)
-        master.maxsize(1000, 1000)
+        master.geometry('410x390')
+        master.resizable(width=FALSE, height=FALSE)
 
-    def create_widgets(self, root):
-        self.button_send = tk.Button(self)
-        self.button_send["text"] = "Hello World\n(click me)"
-        self.button_send["command"] = self.say_hi
-        self.button_send.pack(side="top")
-        self.button_quit = tk.Button(self, text="QUIT", fg="red", command=root.destroy)
-        self.button_quit.pack(side="bottom")
+    def create_widgets(self):
+        self.chat_log = Text(self, bd=0, bg='white', height=17, width='45', font='Arial', state=DISABLED)
+        self.chat_log.insert(END, 'nlkkj\ndfg\ndfg\ndfg\n')
+        self.chat_entry = Text(self, bd=0, bg='white', height=3, width='35', font='Arial')
+        self.button_send = Button(self, text="Send", width=7, command=self.send_message)
+        self.button_quit = Button(self, text="Connect", width=7, fg="red", command=self.connect_server)
 
-    @classmethod
-    def say_hi(cls):
-        print("hi there, everyone!")
+        self.chat_log.grid(row=0, column=0)
+        self.chat_entry.grid(row=1, column=0, sticky=(N, W))
+        self.button_send.grid(row=1, column=0, sticky=(N,E))
+        self.button_quit.grid(row=1, column=0, sticky=(S,E))
+
+    def send_message(self):
+        print("YOU: " + self.chat_entry.get("0.0", END))
+        self.chat_log.config(state=NORMAL)
+        self.chat_log.insert(END, "YOU: " + self.chat_entry.get("0.0", END))
+        self.chat_log.config(state=DISABLED)
+
+    def load_message(self, msg):
+        self.chat_log.config(state=NORMAL)
+        self.chat_log.insert(END, "YOU: " + msg)
+        self.chat_log.config(state=DISABLED)
+
+    def connect_server(self):
+        pass
