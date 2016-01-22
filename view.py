ae00646806aa63dfd9aa57bf1e00122626197252
Piotr Ceranek <petr.ceranek@gmail.com>
2016-01-23 00:56:03 +0100

    GUI development 1.1

--- view.py
+++ view.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 from tkinter import *
+from sockets import *
+from steganography import *
 __author__ = 'piotrowy'
 
 
@@ -18,27 +20,33 @@ class Chat(tk.Frame):
         master.resizable(width=FALSE, height=FALSE)
 
     def create_widgets(self):
-        self.chat_log = Text(self, bd=0, bg='white', height=17, width='45', font='Arial', state=DISABLED)
-        self.chat_log.insert(END, 'nlkkj\ndfg\ndfg\ndfg\n')
-        self.chat_entry = Text(self, bd=0, bg='white', height=3, width='35', font='Arial')
+        self.chat_log = Text(self, bd=0, bg='white', height=17, width='43', font='Arial', state=DISABLED)
+        self.scrollbar = Scrollbar(self, command=self.chat_log.yview)
+        self.chat_entry = Text(self, bd=0, bg='white', height=3, width='33', font='Arial')
         self.button_send = Button(self, text="Send", width=7, command=self.send_message)
         self.button_quit = Button(self, text="Connect", width=7, fg="red", command=self.connect_server)
 
         self.chat_log.grid(row=0, column=0)
+        self.scrollbar.grid(row=0, column=1, sticky=(N, E))
         self.chat_entry.grid(row=1, column=0, sticky=(N, W))
-        self.button_send.grid(row=1, column=0, sticky=(N,E))
-        self.button_quit.grid(row=1, column=0, sticky=(S,E))
+        self.button_send.grid(row=1, column=0, sticky=(N, E))
+        self.button_quit.grid(row=1, column=0, sticky=(S, E))
 
-    def send_message(self):
-        print("YOU: " + self.chat_entry.get("0.0", END))
+        self.chat_entry.bind("<Return>", self.send_message)
+        self.chat_entry.bind("<KeyRelease-Return>", self.clear_entry)
+
+    def send_message(self, event=None):
         self.chat_log.config(state=NORMAL)
         self.chat_log.insert(END, "YOU: " + self.chat_entry.get("0.0", END))
         self.chat_log.config(state=DISABLED)
 
-    def load_message(self, msg):
+    def load_message(self, message):
         self.chat_log.config(state=NORMAL)
-        self.chat_log.insert(END, "YOU: " + msg)
+        self.chat_log.insert(END, "YOU: " + message)
         self.chat_log.config(state=DISABLED)
 
     def connect_server(self):
         pass
+
+    def clear_entry(self, event=None):
+        self.chat_entry.delete("0.0", END)
