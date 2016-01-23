6115f7bbcad854be7e8aade3a2405d8137502ff5
Piotr Ceranek <petr.ceranek@gmail.com>
2016-01-24 00:38:54 +0100

    encode_secret_message function added

--- client/steganography.py
+++ client/steganography.py
@@ -6,8 +6,12 @@ __author__ = 'piotrowy'
 lena_image = [[]]
 
 
-def encode_secret_message(msg):
-    pass
+def encode_secret_message(img):
+    out_string = ''
+    for i in range(len(lena_image)):
+        for j in range(len(lena_image[i])):
+            out_string += str(img[i][j])
+    return out_string + '\n'
 
 
 def decode_secret_message(msg):
@@ -21,6 +25,7 @@ def encode_to_sockets(str_table):
 def decode_from_sockets(socket_string):
     data = [[]]
     index = 0
+    socket_string = socket_string[:len(socket_string)-1]
     for i in range(len(lena_image)):
         for j in range(len(lena_image[i])):
             data[i][j] = int(socket_string[index])
