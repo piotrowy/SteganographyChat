62e1ce8964105089500cdb2f35bf530d80485917
Michał Chatłas <michal.chatlas@espeo.pl>
2016-01-23 17:03:17 +0100

    Simple tcp server running on server side

--- server/server.cpp
+++ server/server.cpp
@@ -1,11 +1,19 @@
-#include <cstdlib>
+#include <unistd.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <arpa/inet.h>
 #include <netdb.h>
-#include <iostream>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <time.h>
 #include <ctime>
+
+#define SERVER_PORT 1234
+#define QUEUE_SIZE 5
 
 class Message {
 
@@ -52,21 +60,47 @@ class History {
 
 int main() {
 
-    int** dupa;
-    dupa = new int*[512];
-    for (int i = 0; i <512; i++) {
-        dupa[i] = new int[512];
-    }
-    dupa[3][4] = 33;
+   int nSocket, nClientSocket;
+   int nBind, nListen;
+   int nFoo = 1;
+   socklen_t nTmp;
+   struct sockaddr_in stAddr, stClientAddr;
 
-    Message* test = new Message(dupa);
-    test->get_lena()[123][7] = 666;
 
-    History* common_message_history = new History();
+   /* address structure */
+   memset(&stAddr, 0, sizeof(struct sockaddr));
+   stAddr.sin_family = AF_INET;
+   stAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+   stAddr.sin_port = htons(SERVER_PORT);
 
-    for (int i = 0; i < 1123; i++) {
-        common_message_history->insert_message(*test);
-    }
+   /* create a socket */
+   nSocket = socket(AF_INET, SOCK_STREAM, 0);
+   setsockopt(nSocket, SOL_SOCKET, SO_REUSEADDR, (char*)&nFoo, sizeof(nFoo));
 
-    std::cout<<common_message_history->get_newest_index()<<std::endl<<common_message_history->get_message(3).get_lena()[123][7];
+   /* bind a name to a socket */
+   nBind = bind(nSocket, (struct sockaddr*)&stAddr, sizeof(struct sockaddr));
+
+   /* specify queue size */
+   nListen = listen(nSocket, QUEUE_SIZE);
+
+   while(1)
+   {
+       /* block for connection request */
+       nTmp = sizeof(struct sockaddr);
+       nClientSocket = accept(nSocket, (struct sockaddr*)&stClientAddr, &nTmp);
+
+           printf("[connection from: %s]\n", inet_ntoa((struct in_addr)stClientAddr.sin_addr));
+           time_t now;
+           struct tm *local;
+           time (&now);
+           local = localtime(&now);
+           char buffer[50];
+           int n;
+           n = sprintf(buffer, "%s\n", asctime(local));
+           write(nClientSocket, buffer, n);
+           close(nClientSocket);
+       }
+
+   close(nSocket);
+   return(0);
 }
