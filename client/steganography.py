b3c889907e72ce31687f4b4feb3b4286d0da5087
Piotr Ceranek <petr.ceranek@gmail.com>
2016-01-24 00:35:37 +0100

    decode_from_sockets function added

--- client/steganography.py
+++ client/steganography.py
@@ -3,6 +3,8 @@
 import skimage as skm
 __author__ = 'piotrowy'
 
+lena_image = [[]]
+
 
 def encode_secret_message(msg):
     pass
@@ -12,9 +14,15 @@ def decode_secret_message(msg):
     pass
 
 
-def encode_to_sockets(img):
+def encode_to_sockets(str_table):
     pass
 
 
-def decode_from_sockets(img):
-    pass
+def decode_from_sockets(socket_string):
+    data = [[]]
+    index = 0
+    for i in range(len(lena_image)):
+        for j in range(len(lena_image[i])):
+            data[i][j] = int(socket_string[index])
+            index += 1
+    return data
